e6468b3edbc678d40155a3b3b2a7c218007ec7f7
Kobe Cao <1957365020@qq.com>
2020-11-25 20:16:18 +0800

    vue

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -4,11 +4,16 @@
     <!-- 轮播图组件 -->
     <home-swiper :banners="banners"></home-swiper>
     <!-- 推荐信息的展示 -->
-    <recommend-view  :recommends="recommends"></recommend-view>
+    <recommend-view :recommends="recommends"></recommend-view>
     <!-- 本周流行 -->
     <feature-view></feature-view>
     <!-- 商品分类 -->
-    <tab-control :titles="['流行','新款','精选']" class="tab-control"></tab-control>
+    <tab-control
+      :titles="['流行', '新款', '精选']"
+      class="tab-control">
+    </tab-control>
+    <!-- 商品数据展示 -->
+    <good-list :goods="goods['pop'].list"></good-list>
     <ul>
       <li>列表1</li>
       <li>列表2</li>
@@ -114,7 +119,6 @@
   </div>
 </template>
 <script>
-
 //子组件
 import HomeSwiper from "./childComps/HomeSwiper";
 import RecommendView from "./childComps/RecommendView";
@@ -122,9 +126,11 @@ import FeatureView from "./childComps/FeatureView";
 
 // 公共的组件
 import NavBar from "components/common/navbar/NavBar";
-import TabControl from "components/content/tabControl/TabControl"
+import TabControl from "components/content/tabControl/TabControl";
+import GoodList from "components/content/goods/GoodsList"
 // 方法
-import { getHomeMultidata } from "network/home";
+import { getHomeMultidata, getHomeGoods } from "network/home";
+
 
 export default {
   name: "Home",
@@ -133,23 +139,47 @@ export default {
     RecommendView,
     FeatureView,
     NavBar,
-    TabControl
+    TabControl,
+    GoodList
   },
   data() {
     return {
       banners: [],
       recommends: [],
+      goods: {
+          'pop': {page: 0, list: []},
+          'new': {page: 0, list: []},
+          'sell': {page: 0, list: []},
+        },
     };
   },
   created() {
     // 1.请求多个数据
-    getHomeMultidata().then((res) => {
-      console.log(res);
-      // 将请求的数据保存到data()里面
-      // this.result = res;
-      this.banners = res.data.data.banner.list;
-      this.recommends = res.data.data.recommend.list;
-    });
+    this.getHomeMultidata();
+    // 2.请求商品数据
+      this.getHomeGoods('pop')
+      this.getHomeGoods('new')
+      this.getHomeGoods('sell')
+  },
+  methods: {
+    getHomeMultidata() {
+      getHomeMultidata().then((res) => {
+        // 将请求的数据保存到data()里面
+        // this.result = res;
+        this.banners = res.data.data.banner.list;
+        this.recommends = res.data.data.recommend.list;
+      });
+    },
+    getHomeGoods(type) {
+      // 动态获取页码
+      const page = this.goods[type].page + 1;
+      getHomeGoods(type, page).then((res) => {
+        // 将data里面的数据取出来保存到goods里面的list[]数组里面
+        this.goods[type].list.push(...res.data.data.list);
+        // 更新页码
+        this.goods[type].page += 1;
+      });
+    },
   },
 };
 </script>
@@ -170,5 +200,6 @@ export default {
 .tab-control {
   position: sticky;
   top: 44px;
+  z-index: 9;
 }
 </style>
